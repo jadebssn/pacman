467ebd69afd551b987ed555e265fac2e557c9744
Ungala <72914489+ungala1@users.noreply.github.com>
2021-05-21 13:39:03 +0400

    close mouse

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
 'use strict';
 
 console.log('it works!');
+
+const pacman = document.querySelector('.entity--pac');
+
+
+document.addEventListener('click', () => {
+    console.log('click');
+    pacman.classList.toggle('pacboy--close-mouse')
+
+})
