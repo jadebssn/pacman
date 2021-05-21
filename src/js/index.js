012037f8bf5b44527c1cfef2adbf8f904df86d5d
Jade <jade.besson@hotmail.fr>
2021-05-21 11:46:48 +0200

    changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
 'use strict';
 
-console.log('it works!');
+// console.log('it works!');
+
+const pacman = document.querySelector('.entity entity--pac pacboy-active-light');
+console.log(pacman);
+document.addEventListener('click', () => {
+const closed = document.querySelector('.entity entity--pac pacboy-active-light');
+closed.className = ''
+})
